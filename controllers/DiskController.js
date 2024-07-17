cae845f8da0097163af8dc4427b7df72253be7f6
JunhyV <jonhy.kisi@gmail.com>
2024-07-17 14:35:33 -0600

    verificar datos

--- controllers/DiskController.js
+++ controllers/DiskController.js
@@ -2,13 +2,12 @@ const Disk = require('../models/Disk');
 
 // Add register
 exports.newDisk = async (req, res) => {
-    console.log(req.body);
     const disk = new Disk(req.body);
 
     try {
         // Save register
         await disk.save();
-        res.json({mensaje: 'New disk registered'});
+        res.json({mensaje: 'New disk registered', datos: req.body});
         
     } catch (error) {
         // Log y Next
