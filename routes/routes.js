a3847c6ddfd3bf9cacb0051cb677c002f42d4f90
JunhyV <jonhy.kisi@gmail.com>
2024-07-19 08:08:20 -0600

    back works

--- routes/routes.js
+++ routes/routes.js
@@ -5,7 +5,19 @@ const disksRoutes = require('./disksRoutes');
 router.use('/', disksRoutes);
 
 router.get('/', (req, res) => {
-  res.send('Lista de APIs');
+  res.send(`
+    <html>
+      <head>
+        <title>NodeJs y Express en Vercel</title>
+      </head>
+      <body>
+        <h1>List of API's:</h1>
+        <ul>
+        <li><a href="/disks">Drive Disk Set API</a></li>
+        </ul>
+      </body>
+    </html>
+  `);
 });
 
 module.exports = router
